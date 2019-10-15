3423600e57b3c1fe5dfd005996c750d2128b4471
@xanderlav <alejandroregino.hussmann.com>
2019-10-15 03:07:22 -0500

    API connection

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,9 @@ var txtSearch = "";
 var idBtn= "gifSrch_";
 var arrBotns = [];
 var cont = 0;
+var apiKy = "2duYbYAookqwoNvzsKeis23nvpvStsJV";
+var limite = 10;
+var qrySearch = "";
 
 
 $('#buttons').on("click", function() {
@@ -10,18 +13,29 @@ $('#buttons').on("click", function() {
 
 
 $('#gifBtn').on("click", function() {
-    if(txtSearch = $("#mySearch").val().trim()){
-        cont++;
-        var btn = $("<button>");
-        btn.attr("type", "button");
-        btn.attr("class", "btn btn-primary btn-lg");
-        btn.attr("id", idBtn + cont);
-        // btn.attr("value", txtSearch);
-        $(btn).html(txtSearch);
-        $('#buttons').prepend(btn);
-        arrBotns.push(txtSearch);
-        // console.log("<--" + arrBotns);
-        $("#mySearch").empty();
-    };
+    // if(txtSearch = $("#mySearch").val().trim()){
+    //     cont++;
+    //     var btn = $("<button>");
+    //     btn.attr("type", "button");
+    //     btn.attr("class", "btn btn-primary btn-lg");
+    //     btn.attr("id", idBtn + cont);
+    //     // btn.attr("value", txtSearch);
+    //     $(btn).html(txtSearch);
+    //     $('#buttons').prepend(btn);
+    //     arrBotns.push(txtSearch);
+    //     // console.log("<--" + arrBotns);
+    //     $("#mySearch").empty();
+    // };
+    getGiphy();
 });
 
+function getGiphy(){
+
+//javascript, jQuery
+qrySearch = $("#mySearch").val().trim();
+
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + qrySearch + "&api_key=" + apiKy + "&limit" + limite);
+console.log("your request was: " + xhr);
+
+xhr.done(function(response) { console.log("Success got answer: ", response); });
+}
