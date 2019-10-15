9e25f15c85b95d29a4718ba8d673ab54d1b60520
@xanderlav <alejandroregino.hussmann.com>
2019-10-15 01:09:17 -0500

    testing functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,27 @@
 var txtSearch = "";
+var idBtn= "gifSrch_";
+var arrBotns = [];
+var cont = 0;
 
 
-$("#gifBtn").on("click", function(){
-    alert(txtSearch = $("#mySearch").val());
-})
+$('#buttons').on("click", function() {
+    console.log("ahora esto " + $(this).attr("outerText"));
+});
+
+
+$('#gifBtn').on("click", function() {
+    if(txtSearch = $("#mySearch").val().trim()){
+        cont++;
+        var btn = $("<button>");
+        btn.attr("type", "button");
+        btn.attr("class", "btn btn-primary btn-lg");
+        btn.attr("id", idBtn + cont);
+        // btn.attr("value", txtSearch);
+        $(btn).html(txtSearch);
+        $('#buttons').prepend(btn);
+        arrBotns.push(txtSearch);
+        // console.log("<--" + arrBotns);
+        $("#mySearch").empty();
+    };
+});
+
